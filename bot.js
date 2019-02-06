bc9095c69eca803566b7e36476a0e16ca1a57967
NaoiGnzalez <47344888+NaoiGnzalez@users.noreply.github.com>
2019-02-06 03:04:10 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,15 @@ bot.on("ready", function() {
     bot.user.setGame(`Follow me on IG: Naoi_Gnzalez`);
     console.log(`${bot.user.username} Esta Listo!`);
 });
-
+//CUAK-
+ "embed": {
+    "color": 15743107,
+    "image": {
+      "url": `${member.user.avatarURL}`
+    },
+    "description": `** Bip bop! Welcome to TMesG! ${member.user.tag}!!! <3**   `,
+  }
+    //CUAK
 bot.on("message", function(message) {
 
     if (message.author.bot) return;
@@ -39,16 +47,7 @@ bot.on("message", function(message) {
         if(message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(embedhelpadmin);
     };
 
-    if (command == "avatar") {
-        let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        message.channel.send({
-               embed: {
-                  title: `${member.displayAvatarURL}'s Profile Picture.`,
-                  image: {
-                      url: member.AvatarURL
-                  },
-                  color: 0x00FFEE
-               }
+
         })
     };
 
@@ -56,35 +55,8 @@ bot.on("message", function(message) {
         message.channel.send("**:ping_pong: PONG!**");
     };
 
-    if(command === "prune") {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("**ðŸ”’ Sorry, you can't do that.**");
-        var messagesToDelete = args[1];
-        if (!args[1]) return message.channel.send("âŒ Please include the amount of Message that you want to **Prune**!");
-        if (args[1] > 99) return message.channel.send("âŒ I can't **Prune** more than `99` Messages.");
-        message.channel.fetchMessages({limit: messagesToDelete})
-        .then(messages => message.channel.bulkDelete(messages.size + 1))
-        .catch(error => message.channel.send(`âŒ Sorry ${message.author}, Failed while **Prunning** because: *${error}*.`));
-    };
+  
 
-    if(command == "kick") {
-        message.delete()
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("âŒ Please **@mention** your target!");
-        let kReason = args.join(" ").slice(0);
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("**ðŸ”’ Sorry, you can't do that.**");
-        if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("âŒ Failed to **Kick**, need a higher than Roles.");
-    
-        let kickEmbed = new Discord.RichEmbed()
-        .setDescription("**ðŸ‘¢ Kicked**")
-        .setColor(0xFF0000)
-        .addField("User", `${kUser}`)
-        .addField("Moderator", `<@${message.author.id}>`)
-        .addField("Reason", `**\`\`\`${kReason}\`\`\`**`);
-    
-        let adminlog = message.guild.channels.find(`name`, "mod-logs");
-        if(!adminlog) return message.channel.send("âŒ Sorry, i need the Logging Channels with name **#mod-logs**.");
-        message.guild.member(kUser).kick(kReason);
-        adminlog.send(kickEmbed);
     };
 
 });
