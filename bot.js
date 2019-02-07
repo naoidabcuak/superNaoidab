29dcd71247aa97a7740d0d62bc461bfc50c52df3
NaoiGnzalez <47344888+NaoiGnzalez@users.noreply.github.com>
2019-02-07 01:13:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,27 +2,63 @@ const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
 const PREFIX = process.env.PREFIX;
 
-var bot = new Discord.Client();
+var DiscordClient = require('discord.io');
+var nodeSpotifyWebHelper = require('node-spotify-webhelper');
 
-// Events.
-bot.on("ready", function() {
-    bot.user.setGame(`Follow me on IG: Naoi_Gnzalez`);
-    console.log(`${bot.user.username} Esta Listo!`);
+var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
+var config = require("./config");
+var bot = new DiscordClient({
+    autorun: true,
+    email: config.email,
+    password: config.password,
+    username: config.username,
+    token: config.token
+});
 
-// Commands.
-    if (command == "help") {
-        var embedhelpmember = new Discord.RichEmbed()
-            .setAuthor("Command List.")
-            .addField(" - ping", "CUAK")
-            .setColor(0x00FFEE)
-    };
-
-
-    if (command == "Cuak") {
-        message.channel.send("**TMesG**");
+bot.on('ready', function () {
+    console.log(bot.username + " - (" + bot.id + ")");
+    bot.on('disconnected', function () {
+        setTimeout(function () {
+            bot.connect();
+        }, 5000);
+    });
 
 });
 
-// Bot Login.
-// bot.login('BOT_TOKEN');
-bot.login(process.env.BOT_TOKEN);
+var curr = "";
+// get the name of the song which is currently playing 
+setInterval(function () {
+    spotify.getStatus(function (err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        if (res.playing) {
+            var next = "♪" + res.track.track_resource.name + " - " + res.track.artist_resource.name + "♪";
+            if (next == curr) return;
+            //console.log(next);
+            bot.setPresence({
+                game: next
+            });
+            curr = next;
+        } else {
+            curr = "";
+            bot.setPresence({
+                game: ""
+            });
+        }
+    })
+}, 3000);
+
+bot.on('message', function (_user, userID, channelID, message, rawEvent) {
+    if (message == (bot.username + ", music link?")) {
+        spotify.getStatus(function (err, res) {
+            if (err) {
+                return console.error(err);
+            }
+            bot.sendMessage({
+                to: channelID,
+                message: "http://open.spotify.com/" + res.track.track_resource.uri.split(":").slice(1).join("/")
+            })
+        });
+    }
+});
